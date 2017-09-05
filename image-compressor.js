52550ce52c0c5e2ff4db76eddf875a488852fb70
Oleh <powerbot15@gmail.com>
2017-09-05 18:03:06 +0300

    optimizations

--- image-compressor.js
+++ image-compressor.js
@@ -1,10 +1,8 @@
 /**
  * Author: Oleh Kastornov
- * ver 1.1.6
+ * ver 1.2.0
  * Ukraine
  */
-
-
 (function(win, doc){
     var COMPRESSOR_DEFAULTS = {
         toWidth : null,
@@ -15,47 +13,29 @@
         grayScale : false,
         sepia : false,
         threshold : false,
+        vReverse : false,
+        hReverse : false,
         quality : 1
     };
     function ImageCompressor () {
-
         this.settings = JSON.parse(JSON.stringify(COMPRESSOR_DEFAULTS));
-
         this.canvas = doc.createElement('canvas');
         this.image = doc.createElement('img');
         this.context = this.canvas.getContext('2d');
-
         this.listenEvents();
     }
-
     ImageCompressor.prototype = {
-
         listenEvents : function () {
             this.image.addEventListener('load', this.imageLoaded.bind(this), false);
             this.image.addEventListener('error', this.imageFault, false);
         },
-
         imageLoaded : function () {
-
             this.checkSizes();
-
-            if(this.settings.speed == 'low'){
-
-                this.compressWithQuality();
-            }
-
-            if(this.settings.speed == 'high'){
-
-                this.compressImage();
-
-            }
-
+            this.compress();
         },
-
         imageFault : function () {
             throw new Error('Source can not be loaded');
         },
-
         run : function (src, settings, callback) {
             this.settings = JSON.parse(JSON.stringify(COMPRESSOR_DEFAULTS));
             this.settings.toWidth = settings.toWidth; //|| this.settings.toWidth;
@@ -69,110 +49,56 @@
             this.imageReceiver = callback;
             this.image.src = src;
         },
-
-        compressImage : function () {
-
+        compress : function () {
+            if(this.settings.speed !== 'high'){
+                this.compressWithQuality();
+            }
             this.naturalAR = this.image.naturalWidth / this.image.naturalHeight;
             this.compressedAR = this.settings.toWidth / this.settings.toHeight;
             this.canvas.width = this.settings.toWidth;
             this.canvas.height = this.settings.toHeight;
             this.context.fillStyle = this.settings.mimeType == 'image/png' ? 'rgba(255, 255, 255, 0)' : '#FFFFFF';
             this.context.fillRect(0, 0, this.settings.toWidth, this.settings.toHeight);
-
             if(this.settings.mode == 'strict'){
                 this.strictResize();
             }
             if(this.settings.mode == 'stretch'){
                 this.stretchResize();
             }
-
             this.imageFilters();
-
             this.imageReceiver(this.canvas.toDataURL(this.settings.mimeType, this.settings.quality));
-
             this.compressedCanv = null;
-
         },
-
         compressWithQuality : function () {
-
-            if(this.image.naturalWidth < this.settings.toWidth && this.image.naturalHeight < this.settings.toHeight){
-
+            if(this.image.naturalWidth <= this.settings.toWidth && this.image.naturalHeight <= this.settings.toHeight){
                 this.compressImage();
-
                 return;
-
             }
-
             this.firstCompress();
-
-            this.naturalAR = this.image.naturalWidth / this.image.naturalHeight;
-            this.compressedAR = this.settings.toWidth / this.settings.toHeight;
-            this.canvas.width = this.settings.toWidth;
-            this.canvas.height = this.settings.toHeight;
-            this.context.fillStyle = this.settings.mimeType == 'image/png' ? 'rgba(255, 255, 255, 0)' : '#FFFFFF';
-            this.context.fillRect(0, 0, this.settings.toWidth, this.settings.toHeight);
-
-            if(this.settings.mode == 'strict'){
-                this.strictResize();
-            }
-            if(this.settings.mode == 'stretch'){
-                this.stretchResize();
-            }
-
-            this.imageFilters();
-
-            this.imageReceiver(this.canvas.toDataURL(this.settings.mimeType, this.settings.quality));
-
-            this.compressedCanv = null;
-
         },
-
         firstCompress : function () {
             var canvas = doc.createElement('canvas');
-
             var context = canvas.getContext('2d');
-
             var w = this.image.naturalWidth / 2;
-
             var h = this.image.naturalHeight / 2;
-
             var toW = this.settings.toWidth, toH = this.settings.toHeight;
-
             if(w < toW || h < toH){
-
                 return;
-
             }
-
             canvas.width = w;
-
             canvas.height = h;
-
             context.drawImage(this.image, 0, 0, w, h);
-
             w = w / 2;
-
             h = h / 2;
-
             while(w > toW && h > toH){
-
                 context.drawImage(canvas, 0, 0, w * 2, h * 2, 0, 0, w, h);
-
                 w = w / 2;
-
                 h = h / 2;
-
             }
-
             this.compressedCanv = canvas;
-
             this.compressedW = w * 2;
-
             this.compressedH = h * 2;
-
         },
-
         strictResize : function () {
             if ( this.naturalAR >= this.compressedAR ) {
                 this.fitWidth();
@@ -181,98 +107,70 @@
                 this.fitHeight();
             }
         },
-
         stretchResize : function () {
             if(this.compressedCanv){
-
                 this.context.drawImage(this.compressedCanv, 0, 0, this.compressedW, this.compressedH, 0, 0, this.settings.toWidth, this.settings.toHeight);
-
             }
             else{
                 this.context.drawImage(this.image, 0, 0, this.settings.toWidth, this.settings.toHeight);
             }
-
         },
-
         fitWidth : function () {
             var compressedHeight = this.settings.toWidth / this.naturalAR,
                 offsetX = 0,
                 offsetY = (this.settings.toHeight - compressedHeight) / 2;
-
             if(this.compressedCanv){
-
                 this.context.drawImage(this.compressedCanv, 0, 0, this.compressedW,  this.compressedH, offsetX, offsetY, this.settings.toWidth, compressedHeight);
-
             }
             else{
-
                 this.context.drawImage(this.image, offsetX, offsetY, this.settings.toWidth, compressedHeight);
-
             }
-
         },
-
         fitHeight : function () {
             var compressedWidth = this.settings.toHeight * this.naturalAR,
                 offsetY = 0,
                 offsetX = (this.settings.toWidth - compressedWidth) / 2;
-
             if(this.compressedCanv){
-
                 this.context.drawImage(this.compressedCanv, 0, 0, this.compressedW, this.compressedH, offsetX, offsetY, compressedWidth, this.settings.toHeight);
-
             }
             else{
-
                 this.context.drawImage(this.image, offsetX, offsetY, compressedWidth, this.settings.toHeight);
-
             }
-
         },
-
         checkSizes : function () {
             var aspectRatio = this.image.naturalWidth / this.image.naturalHeight;
             if(!this.settings.toWidth && !this.settings.toHeight){
-                this.settings.toWidth = 100;
-                this.settings.toHeight = 100;
+                this.settings.toWidth = this.image.naturalWidth;
+                this.settings.toHeight = this.image.naturalHeight;
                 return;
             }
             if(!this.settings.toWidth){
                 this.settings.toWidth = this.settings.toHeight * aspectRatio;
                 return;
             }
-
             if(!this.settings.toHeight){
                 this.settings.toHeight = this.settings.toWidth / aspectRatio;
             }
-
         },
-
         grayScale : function () {
             var imgData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
             var pixels = imgData.data;
             var grayValue;
-
             for (var i = 0, n = imgData.data.length; i < n; i += 4) {
                 if(imgData.data[i + 3] == 0){
                     continue;
                 }
                 grayValue = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3;
-
                 imgData.data[i] = grayValue;
                 imgData.data[i + 1] = grayValue;
                 imgData.data[i + 2] = grayValue;
             }
-
             this.context.putImageData(imgData, 0, 0);
-
         },
-
         sepia : function () {
             var imgData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
             var pixels = imgData.data;
             var grayValue;
-
             for (var i = 0, n = imgData.data.length; i < n; i += 4) {
                 if(imgData.data[i + 3] == 0){
                     continue;
@@ -281,18 +179,14 @@
                 imgData.data[i + 1] = (imgData.data[i] * .349) + (imgData.data[i + 1] *.686) + (imgData.data[i + 2] * .168);
                 imgData.data[i + 2] = (imgData.data[i] * .272) + (imgData.data[i + 1] *.534) + (imgData.data[i + 2] * .131);
             }
-
             this.context.putImageData(imgData, 0, 0);
-
         },
-
         threshold : function () {
             var imgData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height);
             var pixels = imgData.data;
             var averageIntensity;
             var thresholdVal;
             var grayValue;
-
             for (var i = 0, n = imgData.data.length; i < n; i += 4) {
                 if(imgData.data[i + 3] == 0){
                     continue;
@@ -303,13 +197,9 @@
                 imgData.data[i + 1] = thresholdVal;
                 imgData.data[i + 2] = thresholdVal;
             }
-
             this.context.putImageData(imgData, 0, 0);
-
         },
-
         imageFilters : function () {
-
             if(this.settings.grayScale){
                 this.grayScale();
                 return;
@@ -323,9 +213,6 @@
                 return;
             }
         }
-
     };
-
     win.ImageCompressor = ImageCompressor;
-
 })(window, document);
